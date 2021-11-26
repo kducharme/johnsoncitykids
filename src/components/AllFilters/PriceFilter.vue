8b4765103b5f598eb849f1a43d392576b50abaa1
Kyle Ducharme <ducharme.kyle@gmail.com>
2021-11-26 09:10:29 -0500

    broke out filters into separate components

---
+++ src/components/AllFilters/PriceFilter.vue
@@ -0,0 +1,191 @@
+<template>
+  <section class="filter">
+    <article class="input" @click="displayPriceFilter()" id="input__price">
+      <p class="placeholder">Price</p>
+    </article>
+    <article class="structure hide" id="structure__price">
+      <p v-for="price in prices" :key="price" @click="filterByPrice(price)">
+        {{ price }}
+      </p>
+    </article>
+  </section>
+</template>
+
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      prices: {
+        one: "Free",
+        two: "Paid",
+      },
+    };
+  },
+  methods: {
+    removeClearFilter__Price() {
+      document.querySelector(".clear__price").remove();
+    },
+    filterByPrice(price) {
+      this.$store.commit("filterLocations", {
+        price,
+      });
+      const dropdown = document.querySelector("#structure__price");
+      dropdown.classList.toggle("hide");
+
+      const input = document.querySelector("#input__price");
+      input.textContent = this.$store.state.activeFilters.price;
+      if (document.querySelector(".clear__price") === null) {
+        const clearFilters = this.clearPriceFilter();
+        dropdown.appendChild(clearFilters);
+      }
+    },
+    clearPriceFilter() {
+      const clear = document.createElement("article");
+      clear.textContent = "Clear filter";
+      clear.classList.add("clear__price");
+      clear.addEventListener("click", () => {
+        this.resetPriceFilter();
+        this.removeClearFilter__Price();
+        const priceDropdown = document.querySelector("#structure__price");
+        priceDropdown.classList.add("hide");
+
+        const input = document.querySelector("#input__price");
+        input.classList.remove("input__active");
+        input.textContent = "Price";
+      });
+      return clear;
+    },
+    resetPriceFilter() {
+      this.$store.commit("resetPriceFilter");
+      this.$store.commit("filterLocations", {
+        price: undefined,
+      });
+    },
+
+    displayPriceFilter() {
+      const dropdown = document.querySelector("#structure__price");
+      dropdown.classList.toggle("hide");
+
+      const input = document.querySelector("#input__price");
+      input.classList.toggle("input__active");
+    },
+    toggleDropdown() {
+      const dropdown = document.querySelector("#structure__price");
+      dropdown.classList.toggle("hide");
+
+      const input = document.querySelector(".input");
+      input.classList.toggle("input__active");
+    },
+    selectOption(name) {
+      const text = document.querySelector(".placeholder");
+      text.textContent = name;
+      text.classList.add("input__selected");
+      this.toggleDropdown();
+    },
+  },
+  created() {},
+};
+</script>
+
+<style lang="scss">
+.filters {
+  display: flex;
+  flex-direction: row;
+  padding: 16px 0;
+  margin: 0 0 16px;
+}
+
+.filter {
+  margin-right: 12px;
+}
+
+/* Input styling */
+.input {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #e2e2e2;
+  border-radius: 24px;
+  height: 36px;
+  padding: 4px 16px;
+  background-color: white;
+}
+
+.input__active {
+  border: 2px solid #009478 !important;
+  margin: -1px;
+  background: #fafafa;
+}
+
+.input__placeholder {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.input:hover {
+  cursor: pointer;
+  border: 1px solid #b4b4b4;
+}
+
+.placeholder {
+  color: var(--colorPlaceholder);
+}
+
+.input__selected {
+  color: var(--colorPrimaryDark);
+}
+
+/* Dropdown styling */
+.structure {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 280px;
+  overflow: scroll;
+  background-color: white;
+  box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.1);
+  padding: 6px 0;
+  z-index: 9999;
+  position: absolute;
+  margin-top: 16px;
+  border: 1px solid #e2e2e2;
+  border-radius: 3px;
+}
+
+.structure > p {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  margin: 0;
+}
+
+.structure > p:hover {
+  background-color: #fafafa;
+  cursor: pointer;
+}
+
+.hide {
+  display: none;
+}
+
+.clear__type,
+.clear__price {
+  font-weight: 600;
+  font-family: "avenir";
+  color: black;
+  text-decoration: underline;
+  margin: 16px;
+}
+
+.clear__type:hover,
+.clear__price {
+  cursor: pointer;
+  opacity: 0.7;
+}
+</style>;
