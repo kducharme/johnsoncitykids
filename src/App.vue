7d948898a6436abbd26b908e3cb3301f44357652
Kyle Ducharme <ducharme.kyle@gmail.com>
2021-11-27 20:27:37 -0500

    pulling data from airtable

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,10 @@ export default {
     Map,
   },
 
+  mounted() {
+    this.$store.getters.getAirtableLocations;
+  },
+
   data: () => ({
     //
   }),
@@ -35,7 +39,6 @@ export default {
 </script>
 
 <style lang="scss">
-
 body {
   margin: 0;
   font-family: "avenir";
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import locationData from "../../public/db/data/locations.json";
+import Airtable from "airtable";
 
 Vue.use(Vuex)
 
@@ -22,6 +23,48 @@ export default new Vuex.Store({
     getLocations(state) {
       state.allLocations = locationData;
       state.filteredLocations = locationData;
+    },
+    getAirtableLocations() {
+
+      const base = new Airtable({ apiKey: 'keyPnDWTHY6UHf26L' }).base('app3Dn6iVpWym6Uup');
+
+      base('Locations').select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 3,
+        view: "Grid view"
+      }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach((location) => {
+          console.log(location.fields.name);
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+
+      }, function done(err) {
+        if (err) { console.error(err); return; }
+      });
+
+      // const base = new Airtable({ apiKey: 'keyPnDWTHY6UHf26L' }).base(
+      //   "app3Dn6iVpWym6Uup"
+      // );
+      // base("Locations")
+      //   .select({
+      //     view: "Grid view",
+      //   })
+      //   .firstPage(function (err, locations) {
+      //     if (err) {
+      //       console.error(err);
+      //       return;
+      //     }
+      //     locations.forEach((l) => {
+      //       console.log(l);
+      //       // return record.get("Name")
+      //     });
+      //   });
     }
   },
   mutations: {
@@ -86,9 +129,9 @@ export default new Vuex.Store({
           }
         })
       }
-      
+
       // If there aren't any filters applied, then display allLocations
-      
+
       if (this.state.activeFilters.type === undefined && this.state.activeFilters.price === undefined) {
         this.state.filteredLocations = this.state.allLocations;
       }
