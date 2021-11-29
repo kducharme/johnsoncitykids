23b8bd2d7bae242db21ec8a011f2fa3452e34d35
Kyle Ducharme <ducharme.kyle@gmail.com>
2021-11-29 14:51:49 -0500

    fixing security issues

--- src/store/index.js
+++ src/store/index.js
@@ -23,9 +23,9 @@ export default new Vuex.Store({
   getters: {
     getAirtableLocations(state) {
 
-      const airtableKey = 'keyPnDWTHY6UHf26L';
+      const AIRTABLEKEY = 'keyPnDWTHY6UHf26L';
 
-      const base = new Airtable({ apiKey: airtableKey }).base('app3Dn6iVpWym6Uup');
+      const base = new Airtable({ apiKey: AIRTABLEKEY }).base('app3Dn6iVpWym6Uup');
 
       base('Locations').select({
         // Selecting the first 25 records in Grid view:
